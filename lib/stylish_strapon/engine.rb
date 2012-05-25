47cb45bff6d1373a508fdf7fc120b1c5d51b7ce9
Kristian Mandrup <kmandrup@gmail.com>
2012-05-25 08:57:37 +0200

    fixed engine

--- lib/stylish_strapon/engine.rb
+++ lib/stylish_strapon/engine.rb
@@ -1,6 +1,6 @@
 module StylishStrapon
   module Rails
-    class Engine < Rails::Engine
+    class Engine < ::Rails::Engine
       isolate_namespace StylishStrapon
       
       # config.generators.integration_tool    :rspec
